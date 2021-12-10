a30884f54913c8910bdd5f15a795ea04f6e9abf0
Spatterjaaay <aja.pursova@gmail.com>
2021-12-09 20:26:43 -0800

    added tests for charged above

--- recurrent_data_analysis_test.rb
+++ recurrent_data_analysis_test.rb
@@ -4,6 +4,23 @@ require_relative 'recurrent_data_analysis'
 
 Minitest::Reporters.use!
 
-describe 'analysis' do
+describe "charged_above" do
+  it "returns the correct number of cars charged above given percentage" do
+    expect(charged_above("0.4", 'ev_data.csv')).must_equal 5
+    expect(charged_above("0.1", 'ev_data.csv')).must_equal 5
+    expect(charged_above("0.0", 'ev_data.csv')).must_equal 5
+    expect(charged_above("1.0", 'ev_data.csv')).must_equal 0
+    expect(charged_above("0.99", 'ev_data.csv')).must_equal 4
+  end
+end
+
+describe "average_daily_miles" do
+  it "returns correct result for a vehicle" do
+    
+  end
+
+  it "returns feedback if no vehicle with user specified ID exists" do
+
+  end
 
 end
