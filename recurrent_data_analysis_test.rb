11001c359ef40dc2f4097eaf61044561f2dbb048
Spatterjaaay <aja.pursova@gmail.com>
2021-12-09 23:13:54 -0800

    added tests for average_daily_miles function

--- recurrent_data_analysis_test.rb
+++ recurrent_data_analysis_test.rb
@@ -26,12 +26,21 @@ describe "charged_above" do
 end
 
 describe "average_daily_miles" do
-  it "returns correct result for a vehicle" do
-    
+  it "returns correct result for a given vehicle" do
+    answers = [["cat-car", 5.357142857142857], ["clown-car", 5.857142857142857]]
+    answers.each { |vehicle, result|
+      expect(average_daily_miles(vehicle, "ev_data.csv")).must_equal result
+    }
   end
 
-  it "returns feedback if no vehicle with user specified ID exists" do
+  it "returns 0 if no vehicle with user specified ID exists" do
+    expect(average_daily_miles("unknown", "ev_data.csv")).must_equal 0
+  end
 
+  it "raises an error for a bad file" do
+    expect {
+      average_daily_miles("cat-car", "data.csv")
+    }.must_raise Errno::ENOENT
   end
 
 end
